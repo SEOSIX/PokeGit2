2af6c6f75d0681184af678619d645dc1c1dfb1f9
SEOSIX <seotoons@gmail.com>
2025-01-21 14:31:39 +0100

    Déplacements + evolution
    j'ai ajouter l'évolution des personnages et la barre d'xp qui augmente de maximum au fils du temps qu'ont évolu

--- Pokemen/Assets/Scripts/GameManager.cs
+++ Pokemen/Assets/Scripts/GameManager.cs
@@ -1,32 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
     public int playerLevel = 1;
     public float gameTime = 0f;
-
+    public Slider xpbarr;
+    public GameObject ws;
+    public Sprite newSprite1;
+    public Sprite newSprite;
+    public Sprite newSprite2;
+    public SpriteRenderer sr;
+    void start()
+    {
+        xpbarr.value = 0;
+        sr = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         gameTime += Time.deltaTime;
         CheckEnemyEvolution();
-    }
-
-    void CheckEnemyEvolution()
-    {
-        if (gameTime > 120f) // AprËs 2 minutes, les ennemis Èvoluent
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Logique pour faire Èvoluer les ennemis
+			xpbarr.value = xpbarr.maxValue;
         }
-    }
 
-    public void LevelUpPlayer()
-    {
-        playerLevel++;
-        RewardPlayer();
+        if (xpbarr.value == xpbarr.maxValue)
+        {
+            playerLevel++;
+            RewardPlayer();
+        }
     }
 
     void RewardPlayer()
     {
-        // Logique pour donner un PokÈhumain ou amÈliorer un existant
+        xpbarr.value = 0;
+        StartCoroutine("anim1");
+    }
+
+    IEnumerator anim1()
+    {
+        ws.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        if (sr.sprite == newSprite)
+        {
+            xpbarr.maxValue = 300;
+            sr.sprite = newSprite2;
+        }
+        if (sr.sprite == newSprite1)
+        {
+            xpbarr.maxValue = 200;
+            sr.sprite = newSprite;
+        }
+        yield return new WaitForSeconds(1.5f);
+        ws.SetActive(false);
+    }
+    
+    void CheckEnemyEvolution()
+    {
+        if (gameTime > 120f) // Apr√®s 2 minutes, les ennemis √©voluent
+        {
+            // Logique pour faire √©voluer les ennemis
+        }
     }
 }
--- Pokemen/Assets/Scripts/Player.cs
+++ Pokemen/Assets/Scripts/Player.cs
@@ -1,13 +1,18 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f; 
     private Rigidbody rb;
+    
+    private SpriteRenderer sr;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -17,5 +22,16 @@ public class Player : MonoBehaviour
 
         Vector3 movement = new Vector3(moveX, 0, moveZ).normalized;
         rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("FlipTrue");
+            sr.flipX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("FlipFalse");
+            sr.flipX = false;
+        }
     }
 }
